cb0dcd2ebc78b9d7678674e7090c615a626fe8a6
Timur Borkhodoev <timur.borkhodoev@mail.utoronto.ca>
2019-12-12 12:48:27 -0500

    wip: styled components

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const FormWrapper = styled.div`
+    width: 100%;
+    height: 100%;
+`;
 
 const Search = () => (
-  <div>
+  <FormWrapper>
         moshi moshi
-  </div>
+  </FormWrapper>
 );
 export default Search;
