7407a09f8c5646c3150626dc0fce4e3db9a035e7
Timur Borkhodoev <timur.borkhodoev@mail.utoronto.ca>
2019-12-12 15:08:06 -0500

    wip: add basic form with some yup validation

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,90 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import styled from 'styled-components';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 const FormWrapper = styled.div`
     width: 100%;
     height: 100%;
+    display: flex;
+    @media only screen  and (min-width : 1224px) {
+        //handle desktops
+    }
+
+    @media only screen and (min-device-width : 320px) and (max-device-width : 480px) {
+        //handle mobile
+    }
 `;
+
+// eslint-disable:
+const SearchForm = ({
+  touched,
+  errors,
+  dirty,
+  isSubmitting,
+  handleChange,
+  handleSubmit,
+  handleReset,
+}) => (
+  <form onSubmit={handleSubmit}>
+    <input
+      id="location"
+      placeholder="City, Neighbourhood or MLS® number"
+      type="text"
+      onChange={handleChange}
+    />
+    <input
+      id="min_price"
+      placeholder="Min Price"
+      type="text"
+      onChange={handleChange}
+    />
+    <input
+      id="max_price"
+      placeholder="Max Price"
+      type="text"
+      onChange={handleChange}
+    />
+    <input
+      id="beds"
+      placeholder="Beds"
+      type="text"
+      onChange={handleChange}
+    />
+    <input
+      id="baths"
+      placeholder="Baths"
+      type="text"
+      onChange={handleChange}
+    />
+    <button type="submit" disabled={isSubmitting}>
+              Submit
+    </button>
+  </form>
+);
+
+SearchForm.propTypes = {
+  // would handle proptypes here
+};
+
+
+const schema = Yup.object().shape({
+  location: Yup.string().required('required'),
+  min_price: Yup.string().required('required'),
+  max_price: Yup.string().required('required'),
+  beds: Yup.string().required('required'),
+  baths: Yup.string().required('required'),
+});
 
 const Search = () => (
   <FormWrapper>
-        moshi moshi
+    <Formik
+      onSubmit={(values) => {
+        alert(JSON.stringify(values, null, 2));
+      }}
+      validationSchema={schema}
+      component={SearchForm}
+    />
   </FormWrapper>
 );
 export default Search;
