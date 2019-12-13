c4645c94293b8bba71c3696c722c0e96d648792c
Timur Borkhodoev <timur.borkhodoev@mail.utoronto.ca>
2019-12-12 21:05:55 -0500

    feat:deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 git worktree add -B gh-pages public
 git config --global user.email "justanotheraction@inthewall.com"
 git config --global user.name "Action Bronson"
+cd $GITHUB_WORKSPACE
 
 yarn run build
+
 cd public
 git add --all && \
 git commit -m "Github Action Build ${GITHUB_SHA} `date +'%Y-%m-%d %H:%M:%S'`" --allow-empty && \
 git remote set-url origin https://${GITHUB_ACTOR}:${PUSH_TOKEN}@github.com/${GITHUB_REPOSITORY}
-git push origin gh-pages:gh-pages
+git push --force origin gh-pages:gh-pages
